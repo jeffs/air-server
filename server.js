b9f9d1161ef7f3bbdfd971ea34d38e594f3aea4f
Jeff Schwab <jeff@unbuggy.com>
2018-07-26 12:14:57 -0400

    Make it work

--- server.js
+++ server.js
@@ -5,8 +5,14 @@ const fetch = require('node-fetch');
 const fs = require('fs');
 const http = require('http');
 const https = require('https');
+const childProcess = require('child_process');
 
 const PORT = 3000;
+const TOKEN = '42';
+const PUBLIC = 'public';
+const OUT = 'out';
+const TMP = 'tmp';
+const PREFIX = `http://localhost:${PORT}`;
 
 const parseProtocol = (url) => {
   if (url.startsWith('http://')) {
@@ -18,13 +24,12 @@ const parseProtocol = (url) => {
   }
 };
 
-const download = async (url, dest) => new Promise((resolve, reject) => {
+const download = (url, dest) => new Promise((resolve, reject) => {
   const protocol = parseProtocol(url);
   const file = fs.createWriteStream(dest);
   protocol.get(url, response => {
     response.pipe(file);
     file.on('finish', () => {
-      console.log(`wrote ${dest}`);
       resolve(dest);
     });
   }).on('error', err => {
@@ -35,51 +40,76 @@ const download = async (url, dest) => new Promise((resolve, reject) => {
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.static(PUBLIC));
 
 const parseParams = (req) => new Promise((resolve, reject) => {
   if (!req.body.token) {
     reject(new Error('missing access token'));
-  } else if (!Array.isArray(req.body.url)) {
+    return;
+  }
+  if (!Array.isArray(req.body.url)) {
     reject(new Error('need at least two files to concatenate'));
-  } else {
-    resolve({
-      token: req.body.token,
-      urls: req.body.url,
-    });
+    return;
   }
+  resolve({
+    token: req.body.token,
+    urls: req.body.url,
+  });
 });
 
-const checkAccessToken = async (token) => new Promise((resolve, reject) => {
-  if (token === '42') {
-    resolve();
-  } else {
+const checkAccessToken = (token) => new Promise((resolve, reject) => {
+  if (token !== TOKEN) {
     reject(new Error('bad access token'));
+    return;
   }
+  resolve();
 });
 
-const fetchInputFiles = (urls) => Promise.all(
-  urls.map((url, i) => download(url, `tmp/${i}.mp4`))
+const fetchMP4Files = (urls) => Promise.all(
+  urls.map((url, i) => download(url, `${TMP}/${i}.mp4`))
 );
+
+const writeFileList = (files) => new Promise((resolve, reject) => {
+  const list = `${TMP}/inputs.txt`;
+  const data = files.map(file => `file '../${file}'\n`).join('');
+  fs.writeFile(list, data, err => {
+    if (err) {
+      reject(err);
+      return;
+    }
+    resolve(list);
+  });
+});
+
+const exec = (command) => new Promise((resolve, reject) => {
+  childProcess.exec(command, err => {
+    if (err) {
+      reject(err);
+      return;
+    }
+    resolve();
+  })
+});
+
+const concatMP4Files = (files) => new Promise((resolve, reject) => {
+  const output = `${OUT}/output.mp4`;
+  writeFileList(files).then(async list => {
+    const command =
+      `ffmpeg -f concat -safe 0 -y -i ${list} -c copy ${PUBLIC}/${output}`;
+    exec(command)
+      .then(() => resolve(output))
+      .catch(err => reject(err));
+  }).catch(err => reject(err));
+});
 
 app.post('/', (req, res) => {
   parseParams(req)
     .then(({ token, urls }) => checkAccessToken(token).then(() => urls))
-    .then(fetchInputFiles)
-    .then(files => {
-      res.send(JSON.stringify(files));
-    }).catch(err => {
-      res.status(500).send(err.message);
-    });
-
-  // check the access token
-  // fetch the input files
-  // write the local paths to a spec for ffmpeg
-  // call ffmpeg, writing the output to a public directory
-  // return the mpeg URL to the client
-
+    .then(fetchMP4Files)
+    .then(concatMP4Files)
+    .then(output => res.send(`${PREFIX}/${output}`))
+    .catch(err => res.status(400).send(err.message));
 });
-
 
 app.listen(PORT);
 console.log(`listening on port ${PORT}`);
